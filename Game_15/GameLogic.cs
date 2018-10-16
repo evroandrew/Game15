336f8224c618214fe7efd0f6baa93c5b4ad42663
volotkov <evroandrew@gmail.com>
2018-10-17 00:41:30 +0300

    1

--- Game_15/GameLogic.cs
+++ Game_15/GameLogic.cs
@@ -127,10 +127,9 @@ namespace Game_15
         public void InitGame()
         {
             int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0 };
-            Shuffle(arr);
-            Check_it(arr);
+            //Shuffle(arr);
+            //Check_it(arr);
             for (int n = 0, i = 0; i < 4; ++i)
-            {
                 for (int j = 0; j < 4; ++j, ++n)
                 {
                     _gameField[j, i] = arr[n];
@@ -140,7 +139,6 @@ namespace Game_15
                         ZeroY = i;
                     }
                 }
-            }
         }
 
         public bool CheckAndGo(int value)
